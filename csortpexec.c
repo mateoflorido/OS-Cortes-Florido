12b0975d5e8208904f7f5448480d8d3a321bf976
dcortesantonio <dcortesantonio@gmail.com>
2019-09-16 00:26:46 -0500

    Add files via upload

--- csortpexec.c
+++ csortpexec.c
@@ -1,25 +1,13 @@
-
+#include "csortp.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+
 #include <string.h>
-
-struct Process
-{
-    char *user[36];
-    char *binary[100];
-    char priority;
-    int nice;
-    unsigned short day;
-    unsigned short month;
-    unsigned short year;
-    unsigned short hour;
-    unsigned short minute;
-    unsigned short second;
-};
-
+#define MAXPROCESS 200000
 int main(int argc, char *argv[])
 {
 
@@ -51,22 +39,23 @@ int main(int argc, char *argv[])
     }
 
     printf("\n compile: %s ,", argv[0]);
-    printf("\ncomand: %s ,", argv[1]);
+
     pid_t pid;
-    int n = 1;
     struct Process *processes;
-    int n = 1, status, num_process = 0;
+    char temp_number[6];
+    char tmp_filename[10];
+    int n = 1, status, num_process = 0, i;
     FILE *nf;
     FILE *tmpf;
     FILE *arrf;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    unsigned short r = 0; 
+    unsigned short r = 0;
 
     if (strcmp(argv[n], "-r") == 0)
     {
-        //Inverse
+        r = 1;
         n++;
     }
 
@@ -81,34 +70,33 @@ int main(int argc, char *argv[])
         {
             break;
         }
+        num_process++;
     }
 
     if (pid == 0)
     {
-        processes = malloc(sizeof(*processes) * 20000);
-        if ((nf = fopen(argv[n], "r")) == NULL)
-        {
-            perror("ERROR OPEN: Reading file. ");
-            return (1);
-        }
-        while ((read = getline(&line, &len, nf)) != -1)
-        {
-            fillProcess(line, &processes[num_process]);
-            num_process++;
-        }
 
-        fclose(nf);
-        
-        // Sort del SO.EXEC
+        strcpy(tmp_filename, "tmp");
+        snprintf(temp_number, 10, "%d", n);
+        strcat(tmp_filename, temp_number);
+        if (r == 0)
+            execl("/usr/bin/sort", "/usr/bin/sort", "-b", "-k4n", "-k", "5.7,5.8", "-k", "5.1,5.2", "-k", "5.4,5.5", "-k", "6.1,6.2", "-k", "6.4,6.5", "-k", "6.7,6.8", argv[n], "-o", tmp_filename, NULL);
+        else
+            execl("/usr/bin/sort", "/usr/bin/sort", "-b", "-k4nr", "-k", "5.7,5.8r", "-k", "5.1,5.2r", "-k", "5.4,5.5r", "-k", "6.1,6.2r", "-k", "6.4,6.5r", "-k", "6.7,6.8r", argv[n], "-o", tmp_filename, NULL);
 
         exit(1);
     }
 
     while ((pid = wait(&status)) != -1)
         ;
-    
-  
-    
-     printf(" PADRE->escribir archivo: %s, %d ,", argv[n], getpid());
-    printf("\n output: %s", argv[n]);
+
+    printf("Numero p %d", num_process);
+    execl("/usr/bin/cat", "/usr/bin/cat", "tmp*", ">>", "tmpfinal", NULL);
+
+    if (r == 0)
+        execl("/usr/bin/sort", "/usr/bin/sort", "-b", "-k4n", "-k", "5.7,5.8", "-k", "5.1,5.2", "-k", "5.4,5.5", "-k", "6.1,6.2", "-k", "6.4,6.5", "-k", "6.7,6.8", "tmpfinal", "-o", argv[n], NULL);
+    else
+        execl("/usr/bin/sort", "/usr/bin/sort", "-b", "-k4nr", "-k", "5.7,5.8r", "-k", "5.1,5.2r", "-k", "5.4,5.5r", "-k", "6.1,6.2r", "-k", "6.4,6.5r", "-k", "6.7,6.8r", "tmpfinal", "-o",  argv[n], NULL);
+
+    return (0);
 }
